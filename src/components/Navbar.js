40561ff36b2e93fe709ecf4c273cb52210614693
mecatesl <cristianrg333@gmail.com>
2020-08-02 22:53:37 -0600

    tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,20 +60,20 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              <Link className="navbar-item" to="/tags/analisis">
+                Análisis
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/tags/datos">
+                Datos
               </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
+              <Link className="navbar-item" to="/tags/redes">
+                Redes
               </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
+              <Link className="navbar-item" to="/tags/tendencias">
+                Tendencias
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+              <Link className="navbar-item" to="/tags/estadisticas">
+                Estadísticas
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
